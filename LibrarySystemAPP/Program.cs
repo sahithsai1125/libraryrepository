ece0b0ad9bf6119011ee67e00d808194c17abdfb
sahithsai1125 <sahithsai1125@gmail.com>
2024-10-02 18:23:57 +0530

    changes modified

--- LibrarySystemAPP/Program.cs
+++ LibrarySystemAPP/Program.cs
@@ -8,9 +8,7 @@ class program
         Book b= new Book();
         AudioBook ab= new AudioBook();
         
-        Console.WriteLine($"check in date: {b.checkin().ToString()} , check out remaining duration: {b.checkout().ToString()} , Due Date is: {b.getdue().ToString()}");
 
-        Console.WriteLine($"LibraryitemID: {ab.LibraryitemID}, name: {ab.name},BorrowerName: {ab.BorrowerName}, BorrowerID: {ab.BorrowerID},checkoutindays: {ab.checkoutindays}, Author: {ab.Author}, noofpages: {ab.noofpages}");
 
         Console.WriteLine($"check in date: {ab.checkin().ToString()} , check out remaining duration: {ab.checkout().ToString()} , Due Date is: {ab.getdue().ToString()}");
 
